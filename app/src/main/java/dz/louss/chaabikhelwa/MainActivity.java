0beb6c71efff8795f312b1b6977ffbbb04bddd87
lx0741 <lx0741@126.com>
2014-11-11 10:51:20 +0100

    cleaned up version 1.2

---
+++ app/src/main/java/dz/louss/chaabikhelwa/MainActivity.java
@@ -0,0 +1,60 @@
+package dz.louss.chaabikhelwa;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.app.Activity;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WebView wv = (WebView) this.findViewById(R.id.webView1);
+
+        String url = "file:///android_asset/www/index.html";
+
+        wv.setWebViewClient(new WebViewClient());
+        wv.getSettings().setJavaScriptEnabled(true); // the application is internet free
+        wv.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ONLY);
+        wv.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        wv.loadUrl(url);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+/*    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && wv.canGoBack()) {
+            wv.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+*/
+}
