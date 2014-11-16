3611ceaace648526185e98d9fcf1f6acb538845a
lx0741 <lx0741@126.com>
2014-11-16 23:10:12 +0100

    v1.1 new features + admob support + content on the internet [dropbox hosted] + custom 404 error page + goback key operational

--- app/src/main/java/dz/louss/chaabikhelwa/MainActivity.java
+++ app/src/main/java/dz/louss/chaabikhelwa/MainActivity.java
@@ -1,31 +1,77 @@
+/*TODO
+* add facebook support
+*   - login and commenting system*/
+
 package dz.louss.chaabikhelwa;
 
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.net.MailTo;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import android.app.Activity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends Activity {
+    private WebView wv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WebView wv = (WebView) this.findViewById(R.id.webView1);
 
-        String url = "file:///android_asset/www/index.html";
+        wv = (WebView) this.findViewById(R.id.webView1);
+        wv.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                wv.loadUrl("file:///android_asset/custom_url_error.htm");
 
-        wv.setWebViewClient(new WebViewClient());
-        wv.getSettings().setJavaScriptEnabled(true); // the application is internet free
-        wv.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ONLY);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("mailto:")) {
+                    MailTo mt = MailTo.parse(url);
+                    Intent i = newEmailIntent(MainActivity.this, mt.getTo(), mt.getSubject(), mt.getBody(), mt.getCc());
+                    startActivity(i);
+                    view.reload();
+                    return true;
+                } else {
+                    view.loadUrl(url);
+                }
+                return true;
+            }
+
+            private Intent newEmailIntent(Context context, String address, String subject, String body, String cc) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[] { address });
+                intent.putExtra(Intent.EXTRA_TEXT, body);
+                intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+                intent.putExtra(Intent.EXTRA_CC, cc);
+                intent.setType("message/rfc822");
+                return intent;
+            }
+        });
+
+        String url = "https://dl.dropboxusercontent.com/u/61258639/chaabilyrics/www/index.html";
+        // String url = "file:///android_asset/www/index.html";
+
+        wv.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+        wv.getSettings().setJavaScriptEnabled(true);
         wv.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+
         wv.loadUrl(url);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -42,7 +88,7 @@ public class MainActivity extends Activity {
         return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
-/*    @Override
+    @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         // Check if the key event was the Back button and if there's history
         if ((keyCode == KeyEvent.KEYCODE_BACK) && wv.canGoBack()) {
@@ -53,5 +99,68 @@ public class MainActivity extends Activity {
         // system behavior (probably exit the activity)
         return super.onKeyDown(keyCode, event);
     }
-*/
+
+    /**
+     * This class makes the ad request and loads the ad.
+     */
+    public static class AdFragment extends Fragment {
+
+        private AdView mAdView;
+
+        public AdFragment() {
+        }
+
+        @Override
+        public void onActivityCreated(Bundle bundle) {
+            super.onActivityCreated(bundle);
+
+            // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
+            // values/strings.xml.
+            mAdView = (AdView) getView().findViewById(R.id.adView);
+
+            // get test ads on a physical device. e.g.
+            // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+            AdRequest adRequest = new AdRequest.Builder()
+                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                    .build();
+
+            // Start loading the ad in the background.
+            mAdView.loadAd(adRequest);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_ad, container, false);
+        }
+
+        /** Called when leaving the activity */
+        @Override
+        public void onPause() {
+            if (mAdView != null) {
+                mAdView.pause();
+            }
+            super.onPause();
+        }
+
+        /** Called when returning to the activity */
+        @Override
+        public void onResume() {
+            super.onResume();
+            if (mAdView != null) {
+                mAdView.resume();
+            }
+        }
+
+        /** Called before the activity is destroyed */
+        @Override
+        public void onDestroy() {
+            if (mAdView != null) {
+                mAdView.destroy();
+            }
+            super.onDestroy();
+        }
+
+    }
+
 }
